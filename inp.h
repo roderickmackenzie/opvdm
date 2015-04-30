d0436a49ae8f8b0cdf5c58bea20fab2ae3f41d1d
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2015-04-30 16:40:43 +0100

    added inp.h header file

--- inp.h
+++ inp.h
@@ -23,11 +23,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void inp_load(char *path, char *file);
-void inp_free();
-void inp_search_double(double *out, char *token);
-void inp_search_int(int *out, char *token);
-void inp_search_string(char *out, char *token);
-char *inp_search(char *token);
-void inp_check(double ver);
-void inp_read_buffer(char **buf, long *len, char *path, char *file);
+struct inp_file {
+	char *data;
+	long fsize;
+	char full_name[1000];
+	int pos;
+	int edited;
+};
+
+void inp_save(struct inp_file *in);
+void inp_init(struct inp_file *in);
+int inp_aes_load(struct inp_file *in, char *path, char *file, char *key);
+void inp_load(struct inp_file *in, char *file);
+void inp_free(struct inp_file *in);
+void inp_search_double(struct inp_file *in, double *out, char *token);
+void inp_search_int(struct inp_file *in, int *out, char *token);
+void inp_search_string(struct inp_file *in, char *out, char *token);
+char *inp_search(struct inp_file *in, char *token);
+void inp_check(struct inp_file *in, double ver);
+void inp_read_buffer(char **buf, long *len, char *full_file_name);
+void inp_reset_read(struct inp_file *in);
+char *inp_get_string(struct inp_file *in);
+char *inp_search_part(struct inp_file *in, char *token);
+void inp_load_from_path(struct inp_file *in, char *path, char *file);
+void inp_replace(struct inp_file *in, char *token, char *text);
