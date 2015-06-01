57926b681797095da16e5015c3a79bdbc1a6d927
Roderick MacKenzie <r.c.i.mackenzie@googlemail.com>
2015-06-01 21:54:19 +0100

    fixed inport export bug

--- dump.h
+++ dump.h
@@ -1,24 +1,3 @@
-//    Organic Photovoltaic Device Model - a drift diffusion base/Shockley-Read-Hall
-//    model for organic solar cells. 
-//    Copyright (C) 2012 Roderick C. I. MacKenzie
-//
-//	roderick.mackenzie@nottingham.ac.uk
-//	www.roderickmackenzie.eu
-//	Room B86 Coates, University Park, Nottingham, NG7 2RD, UK
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License along
-//    with this program; if not, write to the Free Software Foundation, Inc.,
-//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifndef h_dump
 #define h_dump
 #include <frame.h>
@@ -26,25 +5,22 @@
 #include "dump_ctrl.h"
 #include "dynamic_store.h"
 
-void dump_init(struct device *in);
-void dump_load_config(struct device *in);
+void dump_init(struct device* in);
+void dump_load_config(struct device* in);
 void dump_remove_snapshots(struct device *in);
-void dump_dynamic_init(struct dynamic_store *store, struct device *in);
-void dump_dynamic_save(char *outputpath, struct dynamic_store *store);
-void dump_dynamic_add_data(struct dynamic_store *store, struct device *in,
-			   double x_value);
+void dump_dynamic_init(struct dynamic_store *store,struct device *in);
+void dump_dynamic_save(char *outputpath,struct dynamic_store *store);
+void dump_dynamic_add_data(struct dynamic_store *store,struct device *in, double x_value);
 void dump_dynamic_free(struct dynamic_store *store);
-void dump_build_2d_charge_frame(struct map *mapin_e, struct map *mapin_h,
-				struct device *in);
-void dump_write_2d_charge_map(struct map *in_e, struct map *in_h,
-			      struct device *dev);
-void frame_add_data(struct map *in, double x, double y, double data);
-void dump_slice(struct device *in, char *prefix);
-void dump_energy_slice(struct device *in, int num, int i);
+void dump_build_2d_charge_frame(struct map *mapin_e,struct map *mapin_h,struct device *in);
+void dump_write_2d_charge_map(struct map *in_e,struct map *in_h,struct device *dev);
+void frame_add_data(struct map *in,double x,double y,double data);
+void dump_slice(struct device *in,char *prefix);
+void dump_energy_slice(struct device *in,int num,int i);
 
-void dump_write_2d_charge_single_map(struct map *in, struct device *dev);
-void dump_build_2d_charge_single_frame(struct map *mapin, struct device *in);
-void dump_device_map(char *out_dir, char *extra, struct device *in);
-void dump_1d_slice(struct device *in, char *extra);
-void dump_write_to_disk(struct device *in);
+void dump_write_2d_charge_single_map(struct map *in,struct device *dev);
+void dump_build_2d_charge_single_frame(struct map *mapin,struct device *in);
+void dump_device_map(char* out_dir,char* extra,struct device *in);
+void dump_1d_slice(struct device *in,char *extra);
+void dump_write_to_disk(struct device* in);
 #endif
--- inp.c
+++ inp.c
@@ -1,24 +1,3 @@
-//    Organic Photovoltaic Device Model - a drift diffusion base/Shockley-Read-Hall
-//    model for organic solar cells. 
-//    Copyright (C) 2012 Roderick C. I. MacKenzie
-//
-//	roderick.mackenzie@nottingham.ac.uk
-//	www.roderickmackenzie.eu
-//	Room B86 Coates, University Park, Nottingham, NG7 2RD, UK
-//
-//    This program is free software; you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation; either version 2 of the License, or
-//    (at your option) any later version.
-//
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
-//
-//    You should have received a copy of the GNU General Public License along
-//    with this program; if not, write to the Free Software Foundation, Inc.,
-//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,176 +10,237 @@
 #include "code_ctrl.h"
 #include "true_false.h"
 
+#ifdef enable_lock
+#include "./plugins/lock/lock.h"
+#endif
+
 void inp_reset_read(struct inp_file *in)
 {
-	in->pos = 0;
+in->pos=0;
 }
 
-char *inp_get_string(struct inp_file *in)
+char* inp_get_string(struct inp_file *in)
 {
-	int i;
-	static char ret[100];
-	memset(ret, 0, 100);
-	int ii = 0;
-	if (in->pos >= in->fsize) {
-		return NULL;
-	}
+int i;
+static char ret[100];
+memset(ret, 0, 100);
+int ii=0;
+if (in->pos>=in->fsize)
+{
+	return NULL;
+}
 
-	for (i = in->pos; i < in->fsize; i++) {
-		if ((in->data[i] == '\n') || (in->data[i] == 0)) {
-			ret[ii] = 0;
-			in->pos++;
-			break;
-		}
-
-		ret[ii] = in->data[i];
-		ii++;
+for (i=in->pos;i<in->fsize;i++)
+{
+	if ((in->data[i]=='\n')||(in->data[i]==0))
+	{
+		ret[ii]=0;
 		in->pos++;
-
+		break;
 	}
 
-	return ret;
+	ret[ii]=in->data[i];
+	ii++;
+	in->pos++;
+
 }
 
-void inp_read_buffer(char **buf, long *len, char *full_file_name)
+return ret;
+}
+
+
+
+
+void inp_read_buffer(char **buf, long *len,char *full_file_name)
 {
 
-	FILE *f = fopen(full_file_name, "rb");
-	if (f != NULL) {
-		fseek(f, 0, SEEK_END);
-		*len = ftell(f);
-		fseek(f, 0, SEEK_SET);
+//sprintf(full_file_name,"%s/%s",path,file);
+//#ifndef windows
+//if ( access( full_file_name, F_OK ) != -1 )
+FILE *f = fopen(full_file_name, "rb");
+if (f!=NULL)
+{
+	fseek(f, 0, SEEK_END);
+	*len = ftell(f);
+	fseek(f, 0, SEEK_SET);
 
-		*buf = malloc(((*len) + 2) * sizeof(char));
+	*buf = malloc(((*len) + 2)*sizeof(char));
+	memset(*buf, 0, ((*len) + 2)*sizeof(char));
+	fread(*buf, *len, 1, f);
+	fclose(f);
 
-		fread(*buf, *len, 1, f);
-		fclose(f);
+//#ifndef windows
+}else
+{
+	char zip_path[1000];
+	char *file_path=get_dir_name_from_path(full_file_name);
+	char *file_name=get_file_name_from_path(full_file_name);
+//printf("%s %s %s\n",full_file_name,file_path,file_name);
+	sprintf(zip_path,"%s/sim.opvdm",file_path);
+	int err = 0;
+	struct zip *z = zip_open(zip_path, 0, &err);
 
-		(*buf)[(int)*len] = '\n';
-		(*buf)[(int)(*len + 1)] = 0;
+	if (z!=NULL)
+	{
+		//Search for the file of given name
+		struct zip_stat st;
+		zip_stat_init(&st);
+		int ret=zip_stat(z, file_name, 0, &st);
 
-	} else {
-		char zip_path[1000];
-		char *file_path = get_dir_name_from_path(full_file_name);
-		char *file_name = get_file_name_from_path(full_file_name);
+		if (ret==0)
+		{
+			//printf ("Read zip file!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
 
-		sprintf(zip_path, "%s/sim.opvdm", file_path);
-		int err = 0;
-		struct zip *z = zip_open(zip_path, 0, &err);
+			//Alloc memory for its uncompressed contents
+			*len=st.size*sizeof(char);
+			*buf = (char *)malloc(*len);
 
-		if (z != NULL) {
+			//Read the compressed file
+			struct zip_file *f = zip_fopen(z, file_name, 0);
+			zip_fread(f, *buf, st.size);
+			zip_fclose(f);
 
-			struct zip_stat st;
-			zip_stat_init(&st);
-			int ret = zip_stat(z, file_name, 0, &st);
-
-			if (ret == 0) {
-
-				*len = st.size * sizeof(char);
-				*buf = (char *)malloc(*len);
-
-				struct zip_file *f = zip_fopen(z, file_name, 0);
-				zip_fread(f, *buf, st.size);
-				zip_fclose(f);
-
-			} else {
-				ewe("File %s not found\n", file_name);
-			}
-			zip_close(z);
-		} else {
-			ewe("error in config file %s", full_file_name);
+		}else
+		{
+		 	ewe("File %s not found\n", file_name);
 		}
-
+		zip_close(z);
+	}else
+	{
+		ewe("error in config file %s",full_file_name);
 	}
 
+	
 }
+//#endif
+
+}
+
+#ifdef enable_lock
+int inp_aes_load(struct inp_file *in,char *path,char *file,char *key)
+{
+	long int buf_len=0;
+	char *buf;
+
+	sprintf(in->full_name,"%s/%s",path,file);
+
+	FILE *f = fopen(in->full_name, "rb");
+	if (f==NULL)
+	{
+		return -1;
+	}
+	fseek(f, 0, SEEK_END);
+	buf_len = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	buf = malloc((buf_len)*sizeof(char));
+	fread(buf, buf_len, 1, f);
+	fclose(f);
+
+	aes(&(in->data),&(in->fsize),buf,buf_len,FALSE,key);
+	free(buf);
+	//printf("%ld %ld %s\n",buf_len,in->fsize,key);
+	return 0;
+}
+#endif
 
 void inp_init(struct inp_file *in)
 {
-	strcpy(in->full_name, "");
-	in->data = NULL;
-	in->fsize = 0;
-	in->pos = 0;
-	in->edited = FALSE;
+strcpy(in->full_name,"");
+in->data=NULL;
+in->fsize=0;
+in->pos=0;
+in->edited=FALSE;
 }
 
-void inp_load_from_path(struct inp_file *in, char *path, char *file)
+void inp_load_from_path(struct inp_file *in,char *path,char *file)
 {
-	char temp[1000];
-	sprintf(temp, "%s/%s", path, file);
-	inp_load(in, temp);
+char temp[1000];
+sprintf(temp,"%s/%s",path,file);
+inp_load(in,temp);
 }
 
-void inp_load(struct inp_file *in, char *file)
+void inp_load(struct inp_file *in,char *file)
 {
-	in->pos = 0;
+in->pos=0;
 
-	if (strcmp(in->full_name, file) != 0) {
-
-		if (in->data != NULL) {
-			inp_free(in);
-		}
-
-		strcpy(in->full_name, file);
-		inp_read_buffer(&(in->data), &(in->fsize), file);
-		in->edited = FALSE;
+if (strcmp(in->full_name,file)!=0)
+{
+	//printf("Reload %s %s\n",in->full_name,file);
+	if (in->data!=NULL)
+	{
+		inp_free(in);
 	}
+
+	strcpy(in->full_name,file);
+	inp_read_buffer(&(in->data),&(in->fsize),file);
+
+	in->edited=FALSE;
+}
 
 }
 
-void inp_replace(struct inp_file *in, char *token, char *text)
+void inp_replace(struct inp_file *in,char *token, char *text)
 {
-	char *temp = malloc(in->fsize + 100);
-	memset(temp, 0, in->fsize + 100);
-	char *line;
-	int len = 0;
-	line = strtok(in->data, "\n");
-	int found = FALSE;
-	while (line) {
-		if (strcmp(line, token) != 0) {
-			strcat(temp, line);
-			strcat(temp, "\n");
-		} else {
-			strcat(temp, line);
-			strcat(temp, "\n");
-			strcat(temp, text);
-			strcat(temp, "\n");
-			line = strtok(NULL, "\n");
-			found = TRUE;
-		}
-		line = strtok(NULL, "\n");
+char *temp = malloc(in->fsize + 100);
+memset(temp, 0, in->fsize + 100);
+char *line;
+int len=0;
+
+line = strtok(in->data, "\n");
+int found=FALSE;
+
+while(line)
+{
+	if (strcmp(line,token)!=0)
+	{
+		strcat(temp,line);
+		strcat(temp,"\n");
+	}else
+	{
+		strcat(temp,line);
+		strcat(temp,"\n");
+		strcat(temp,text);
+		strcat(temp,"\n");
+		line  = strtok(NULL, "\n");
+		found=TRUE;
 	}
+	line  = strtok(NULL, "\n");
+}
 
-	len = strlen(temp);
-	in->fsize = len;
 
-	if (found == TRUE) {
-		in->edited = TRUE;
-	}
+len=strlen(temp);
+in->fsize=len;
 
-	in->data = realloc(in->data, (len + 1) * sizeof(char));
-	memcpy(in->data, temp, (len + 1) * sizeof(char));
-	free(temp);
+if (found==TRUE)
+{
+	in->edited=TRUE;
+}
+
+in->data=realloc(in->data,(len+1)*sizeof(char));
+memcpy(in->data, temp, (len+1)*sizeof(char));
+free(temp);
 }
 
 void inp_save(struct inp_file *in)
 {
 
-	if (in->edited == TRUE) {
-		int out_fd = open(in->full_name, O_WRONLY | O_CREAT, 0644);
-		if (out_fd == -1) {
-			ewe("File %s can not be opened\n", in->full_name);
+	if (in->edited==TRUE)
+	{
+		int out_fd = open(in->full_name, O_WRONLY | O_CREAT,0644);
+		if (out_fd==-1)
+		{
+			ewe("File %s can not be opened\n",in->full_name);
 		}
 
-		write(out_fd, in->data, in->fsize * sizeof(char));
-
+	    write(out_fd, in->data, in->fsize*sizeof(char));
 		close(out_fd);
+		//printf("%s\n",in->data);
+		in->edited=FALSE;
 
-		in->edited = FALSE;
 	}
 
 }
-
 void inp_free(struct inp_file *in)
 {
 
@@ -210,81 +250,90 @@ void inp_free(struct inp_file *in)
 	inp_init(in);
 }
 
-void inp_search_double(struct inp_file *in, double *out, char *token)
+void inp_search_double(struct inp_file *in,double* out,char* token)
 {
-	sscanf(inp_search(in, token), "%le", out);
+sscanf(inp_search(in,token),"%le",out);
 }
 
-void inp_search_int(struct inp_file *in, int *out, char *token)
+void inp_search_int(struct inp_file *in,int* out,char* token)
 {
-	sscanf(inp_search(in, token), "%d", out);
+sscanf(inp_search(in,token),"%d",out);
 }
 
-void inp_search_string(struct inp_file *in, char *out, char *token)
+void inp_search_string(struct inp_file *in,char* out,char* token)
 {
-	strcpy(out, inp_search(in, token));
+strcpy(out,inp_search(in,token));
 }
 
-void inp_check(struct inp_file *in, double ver)
+void inp_check(struct inp_file *in,double ver)
 {
-	double read_ver = 0.0;
+double read_ver=0.0;
 	inp_reset_read(in);
-	char *line = inp_get_string(in);
-	while (line != NULL) {
+	char *line=inp_get_string(in);
+	while(line!=NULL)
+	{
 
-		if (strcmp(line, "#ver") == 0) {
-			line = inp_get_string(in);
-			sscanf(line, "%le", &read_ver);
+		if (strcmp(line,"#ver")==0)
+		{
+			line  = inp_get_string(in);
+			sscanf(line,"%le",&read_ver);
 
-			if (ver != read_ver) {
-				ewe("File compatability problem %s\n",
-				    in->full_name);
+			if (ver!=read_ver)
+			{
+				ewe("File compatability problem %s\n",in->full_name);
 			}
-			line = inp_get_string(in);
+			line  = inp_get_string(in);
 
-			if ((line == NULL) || (strcmp(line, "#end") != 0)) {
-				ewe("#end token missing %s\n", in->full_name);
+			if ((line==NULL)||(strcmp(line,"#end")!=0))
+			{
+				ewe("#end token missing %s\n",in->full_name);	
 			}
 
 			return;
 		}
 
-		line = inp_get_string(in);
+		line  = inp_get_string(in);
 	}
-	ewe("Token #ver not found in %s\n", in->full_name);
-	return;
+ewe("Token #ver not found in %s\n",in->full_name);
+return;
 }
 
-char *inp_search_part(struct inp_file *in, char *token)
+char* inp_search_part(struct inp_file *in,char *token)
 {
 	inp_reset_read(in);
-	char *line = inp_get_string(in);
-	while (line != NULL) {
-
-		if (cmpstr_min(line, token) == 0) {
+	char * line = inp_get_string(in);
+	while(line!=NULL)
+	{
+		//printf("'%s' '%s'\n", line,token);
+		if (cmpstr_min(line,token)==0)
+		{
 			return line;
 		}
 
-		line = inp_get_string(in);
+		line  = inp_get_string(in);
 	}
 
-	return NULL;
+return NULL;
 }
 
-char *inp_search(struct inp_file *in, char *token)
+char* inp_search(struct inp_file *in,char *token)
 {
 	inp_reset_read(in);
-	char *line = inp_get_string(in);
-	while (line != NULL) {
-
-		if (strcmp(line, token) == 0) {
-			line = inp_get_string(in);
+	char * line = inp_get_string(in);
+	while(line!=NULL)
+	{
+		//printf("'%s' '%s'\n", line,token);
+		if (strcmp(line,token)==0)
+		{
+			line  = inp_get_string(in);
 			return line;
 		}
 
-		line = inp_get_string(in);
+		line  = inp_get_string(in);
 	}
-	ewe("Token %s not found in file %s", token, in->full_name);
-	exit(0);
-	return NULL;
+ewe("Token %s not found in file %s",token,in->full_name);
+exit(0);
+return NULL;
 }
+
+
